ed90032cacdaa508a5c9353fe154128df6d8346f
Van Hung <127950081+xxhumggxx@users.noreply.github.com>
2025-06-25 13:47:17 +0700

    Update ChestV2.lua

--- ChestV2.lua
+++ ChestV2.lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/99fbfd8331645c44"))()
+task.spawn(function()
+    loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/99fbfd8331645c44"))()
+end)
+wait('3')
+task.spawn(function()
+    loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/27bbc5c7202b7722"))()
+end)
